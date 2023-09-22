9875d5abf2b3184c010478c15897af4b6198d6ea
Ana Suryana di PSI <ana.suryana@smt.co.id>
2023-09-22 09:47:25 +0700

    Fix : inventory papper model properties

--- app/Models/InventoryPapper.php
+++ app/Models/InventoryPapper.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class InventoryPapper extends Model
 {
     use HasFactory;
+    protected $table = 'inventory_pappers';
+    protected $fillable = [
+        'nomor_urut', 'item_code', 'item_qty', 'item_box', 'checker_id', 'auditor_id', 'created_by',  'updated_by',
+        'deleted_at', 'deleted_by'
+    ];
 }
