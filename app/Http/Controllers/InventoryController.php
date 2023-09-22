7b773bc2539a73873eec69e08b38c24751df09e3
Mifta <miftaajizulfikar@gmail.com>
2023-09-22 09:37:12 +0700

    Feat WMS-Inventory export to Excel

---
+++ app/Http/Controllers/InventoryController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Reader\Exception;
+use PhpOffice\PhpSpreadsheet\Writer\Xls;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+class InventoryController extends Controller
+{
+    #load data to view
+    function loadInventory(Request $request)
+    {
+        $searchValue = $request->inventory;
+        $Inv = DB::table('WMS_Inv')
+        ->select('cLoc', 'cAssyNo', 'cModel', 'cQty', DB::raw("COUNT(*) as BOX"), DB::raw("SUM(cQty) as Total"))
+        ->groupBy('cLoc','cAssyNo','cModel','cQty')
+        ->paginate(20);
+        return ['data' => $Inv];
+    }
+
+    function formInventory(Request $request)
+    {
+        $searchValue = $request->inventory;
+        $Inv = DB::table('WMS_Inv')
+        ->select('cLoc', 'cAssyNo', 'cModel', 'cQty', DB::raw("COUNT(*) as BOX"), DB::raw("SUM(cQty) as Total"))
+        ->groupBy('cLoc','cAssyNo','cModel','cQty')
+        ->paginate(20);
+        return view('inv_view', ['Inv' => $Inv]);
+    }
+   
+    public function ExportExcel($data_inv)
+        {
+            ini_set('max_execution_time', 0);
+            ini_set('memory_limit', '4000M');
+            try {
+                $spreadSheet = new Spreadsheet();
+                $spreadSheet->getActiveSheet()->getDefaultColumnDimension()->setWidth(20);
+                $spreadSheet->getActiveSheet()->fromArray($data_inv);
+                $Excel_writer = new Xls($spreadSheet);
+                header('Content-Type: application/vnd.ms-excel');
+                header('Content-Disposition: attachment;filename="WMS_Inventory.xls"');
+                header('Cache-Control: max-age=0');
+                ob_end_clean();
+                $Excel_writer->save('php://output');
+                exit();
+            } catch (Exception $e) {
+                return;
+            }
+        }
+        /**
+         *This function loads the customer data from the database then converts it
+         * into an Array that will be exported to Excel
+         */
+        
+        function exportInv(){
+            $data = DB::table('WMS_Inv')
+            ->select('cLoc', 'cAssyNo', 'cModel', 'cQty', DB::raw("COUNT(*) as BOX"),DB::raw("SUM(cQty) as Total"))
+            ->groupBy('cLoc','cAssyNo','cModel','cQty')
+            ->get();
+            $data_array [] = array("cLoc","cAssyNo","cModel","cQty","BOX", "Total");
+            foreach($data as $data_item)
+            {
+                $data_array[] = array(
+                    'Loc' =>$data_item->cLoc,
+                    'Part Code' => $data_item->cAssyNo,
+                    'Part Name' => $data_item->cModel,
+                    'QTY' => $data_item->cQty,
+                    'BOX' => $data_item->BOX,
+                    'Total' => $data_item->Total
+                );
+            }
+            $this->ExportExcel($data_array);
+        }
+}
