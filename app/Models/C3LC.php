4d4cca86a34ae8677a8c303ccd8c8e179eeb1f36
Ana Suryana di PSI <ana.suryana@smt.co.id>
2024-10-24 10:24:30 +0700

    fix : tambah kolom C3LC

--- app/Models/C3LC.php
+++ app/Models/C3LC.php
@@ -10,7 +10,17 @@ class C3LC extends Model
     use HasFactory;
     protected $table = 'C3LC_TBL';
     protected $fillable = [
-        'C3LC_ITMCD', 'C3LC_NLOTNO', 'C3LC_NQTY', 'C3LC_LOTNO', 'C3LC_QTY', 'C3LC_REFF', 'C3LC_LINE',  'C3LC_USRID', 'C3LC_LUPTD',
-        'C3LC_COMID', 'C3LC_NEWID'
+        'C3LC_ITMCD',
+        'C3LC_NLOTNO',
+        'C3LC_NQTY',
+        'C3LC_LOTNO',
+        'C3LC_QTY',
+        'C3LC_REFF',
+        'C3LC_LINE',
+        'C3LC_USRID',
+        'C3LC_LUPTD',
+        'C3LC_COMID',
+        'C3LC_NEWID',
+        'C3LC_DOC'
     ];
 }
