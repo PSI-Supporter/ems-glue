6de73d444c04f82970b2c5e92c2af3124533f8d2
Ana Suryana di PSI <ana.suryana@smt.co.id>
2024-12-04 15:37:58 +0700

    feat : dapatkan detail return dan valuenya

--- app/Http/Controllers/ReturnController.php
+++ app/Http/Controllers/ReturnController.php
@@ -23,21 +23,31 @@ class ReturnController extends Controller
 
     function getCountedPart(Request $request)
     {
+        $additionalWhere = [];
+        $additionalWhere[] = ['RETSCN_SPLDOC', '=', $request->doc];
+
+        if ($request->category) {
+            $additionalWhere[] = ['RETSCN_CAT', '=', $request->category];
+        }
+
+        if ($request->line) {
+            $additionalWhere[] = ['RETSCN_LINE', '=', $request->line];
+        }
+
         $RSSub = DB::table("SPL_TBL")
             ->select("SPL_DOC", "SPL_ITMCD", DB::raw("MAX(SPL_RACKNO) SPL_RACKNO"))
             ->where("SPL_DOC", $request->doc)
             ->groupBy(["SPL_DOC", "SPL_ITMCD"]);
         $RSCountedPart = DB::table("RETSCN_TBL AS a")
-            ->select(DB::raw("a.*,b.*, RTRIM(MITM_SPTNO) MITM_SPTNO, RTRIM(ISNULL(RETSCN_HOLD,'0'))  FLG_HOLD,ISNULL(SPL_RACKNO,'') SPL_RACKNO"))
+            ->select(DB::raw("a.*,b.*, RTRIM(MITM_SPTNO) MITM_SPTNO, RTRIM(ISNULL(RETSCN_HOLD,'0'))  FLG_HOLD,ISNULL(SPL_RACKNO,'') SPL_RACKNO,item_value"))
             ->join("MMADE_TBL AS b", "a.RETSCN_CNTRYID", "=", "MMADE_CD")
             ->join("MITM_TBL", "a.RETSCN_ITMCD", "=", "MITM_ITMCD")
             ->leftJoinSub($RSSub, "dt", function ($join) {
                 $join->on("RETSCN_ITMCD", "=", "SPL_ITMCD")
                     ->on("RETSCN_SPLDOC", "=", "SPL_DOC");
             })
-            ->where("RETSCN_SPLDOC", $request->doc)
-            ->where("RETSCN_CAT", $request->category)
-            ->where("RETSCN_LINE", $request->line)
+            ->leftJoin('raw_material_labels', 'RETSCN_UNIQUEKEY', '=', 'code')
+            ->where($additionalWhere)
             ->orderBy('created_At', 'desc')
             ->get();
         return ['data' => $RSCountedPart];
