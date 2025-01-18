e9fcac9eb89bfbe80ab3372c037cb2efb617cc7f
Ana Suryana di PSI <ana.suryana@smt.co.id>
2025-01-18 11:31:01 +0700

    fix : CT tidak muncul

--- app/Http/Controllers/ProcessMasterController.php
+++ app/Http/Controllers/ProcessMasterController.php
@@ -126,7 +126,7 @@ class ProcessMasterController extends Controller
                         ->whereIn('assy_code', $uniqueAssyCodeList)
                         ->whereDate('valid_date_time', '<=', $dataInput['production_date'])
                         ->orderBy('valid_date_time', 'desc')
-                        ->get(['assy_code', 'cycle_time']);
+                        ->get(['assy_code', 'cycle_time', 'process_code']);
                 }
             }
         } catch (Exception $e) {
