4cc3568c64b1c6659cde4b62a86d0f26b05eac32
Ana Suryana di PSI <ana.suryana@smt.co.id>
2025-02-08 14:17:26 +0700

    feat : keikaku hw migration

---
+++ database/migrations/2025_02_08_070822_create_keikaku_output2s_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keikaku_output2s', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->dateTime('deleted_at')->nullable();
+            $table->date('production_date');
+            $table->dateTime('running_at');
+            $table->string('wo_code', 50);
+            $table->string('line_code', 15);
+            $table->string('process_code', 15);
+            $table->smallInteger('process_seq')->nullable();
+            $table->float('ok_qty', 18, 1);
+            $table->integer('seq_data')->nullable();
+            $table->string('created_by', 9);
+            $table->string('updated_by', 9)->nullable();
+            $table->string('deleted_by', 9)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keikaku_output2s');
+    }
+};
